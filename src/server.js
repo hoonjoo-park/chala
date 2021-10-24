fe42416b331323f28e5267ee215881c2e3f8032c
hoonjoo94 <hoonjoo94@naver.com>
2021-10-25 06:58:14 +0900

    hello

--- src/server.js
+++ src/server.js
@@ -19,7 +19,6 @@ app.use(express.urlencoded({ extended: true }));
 app.use(express.static(path.join(__dirname, 'public')));
 app.use('/convert', express.static('node_modules/@ffmpeg/core/dist'));
 const PORT = process.env.PORT || 7000;
-
 const logger = morgan('dev');
 app.use(logger);
 
